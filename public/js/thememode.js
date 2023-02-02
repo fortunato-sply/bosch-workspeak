e91cdb065138fb0ad8765c0ece147cdbf2c0efa8
lucas <lucas@gmail.com>
2023-02-02 16:51:37 -0300

    add dark and white theme change on button

--- public/js/thememode.js
+++ public/js/thememode.js
@@ -1,7 +1,30 @@
+var value = true;
+
 const icon = document.getElementById('mode-icon');
-console.log(icon);
+
 const switchColorTheme = () => {
-  if (icon.classList.contains('ph-sun')) {
+  if (localStorage.getItem("value") == "false") {
+    icon.classList.replace('ph-sun', 'ph-moon');
+    document.documentElement.style.setProperty('--gray-900', '#e1e1e6');
+    document.documentElement.style.setProperty('--gray-800', '#29292e');
+    document.documentElement.style.setProperty('--gray-700', '#fff');
+    document.documentElement.style.setProperty('--gray-300', '#121214');
+    document.documentElement.style.setProperty('--white', '#111');
+    localStorage.setItem("value", true);
+  }
+  else {
+    icon.classList.replace('ph-moon', 'ph-sun');
+    document.documentElement.style.setProperty('--gray-900', '#121214');
+    document.documentElement.style.setProperty('--gray-800', '#202024');
+    document.documentElement.style.setProperty('--gray-700', '#29292e');
+    document.documentElement.style.setProperty('--gray-300', '#c4c4cc');
+    document.documentElement.style.setProperty('--white', '#fff');
+    localStorage.setItem("value", false);
+  }
+}
+
+const setColorTheme = () => {
+  if (localStorage.getItem("value") == "true") {
     icon.classList.replace('ph-sun', 'ph-moon');
     document.documentElement.style.setProperty('--gray-900', '#e1e1e6');
     document.documentElement.style.setProperty('--gray-800', '#29292e');
--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const sequelize = require('sequelize');
 
 const database = new sequelize('workSpeak', 'Project', 'etsds10243110',
 {
-    dialect: 'mssql', host:'localhost', port: 49701
+    dialect: 'mssql', host:'localhost', port: 1433
 });
 
 database.sync();
